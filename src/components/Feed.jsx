09c92fb5343a7d40af5ec6c9196c766c26b818ee
Shubham Bansal <20eo1057@mitsgwl.ac.in>
2024-05-31 14:47:53 +0530

    updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -24,7 +24,7 @@ const Feed = () => {
       <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
       
       <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-        Copyright © 2022 JSM Media
+        Copyright © Shubham Bansal
       </Typography>
     </Box>
 
